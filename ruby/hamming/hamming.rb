273e0a11aada52ad447a4c9570c8714f015f4e0c
Alexandre Maxime <alexandre.maxime666@gmail.com>
2019-09-17 11:14:09 +0200

    error message change for a better understanding

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,6 +1,6 @@
 class Hamming
   def self.compute(strand1, strand2)
-    raise ArgumentError.new("error, the two DNA don't have the same length") if strand1.length != strand2.length
+    raise ArgumentError.new("error, the two DNA strands don't have the same length") if strand1.length != strand2.length
 
     strand1.each_char.with_index.count { |letter, index|
       letter != strand2[index]
