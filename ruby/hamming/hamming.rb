e451ea6537ceaeb08ad2cdb7b47f3e16c9474659
Alexandre Maxime <alexandre.maxime666@gmail.com>
2019-09-18 08:57:01 +0200

    replace each_car by zip to compare and count

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,8 +2,8 @@ class Hamming
   def self.compute(strand1, strand2)
     raise ArgumentError.new("error, the two DNA strands don't have the same length") if strand1.length != strand2.length
 
-    strand1.each_char.with_index.count { |letter, index|
-      letter != strand2[index]
+    strand1.split(//).zip(strand2.split(//)).count { |letter_strand1, letter_strand2|
+      letter_strand1 != letter_strand2
     }
   end
 end
