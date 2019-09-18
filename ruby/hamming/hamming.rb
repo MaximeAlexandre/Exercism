7b236c5271764a248f171121d98d433916a59b74
Alexandre Maxime <alexandre.maxime666@gmail.com>
2019-09-18 09:26:34 +0200

    guard statement with unless

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,6 +1,6 @@
 class Hamming
   def self.compute(strand1, strand2)
-    raise ArgumentError.new("error, the two DNA strands don't have the same length") if strand1.length != strand2.length
+    raise ArgumentError.new("error, the two DNA strands don't have the same length") unless strand1.length == strand2.length
 
     strand1.split(//).zip(strand2.split(//)).count { |letter_strand1, letter_strand2|
       letter_strand1 != letter_strand2
