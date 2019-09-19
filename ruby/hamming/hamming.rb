5fddd12aee66eaab58d813d1b0b293b88a716905
Alexandre Maxime <alexandre.maxime666@gmail.com>
2019-09-19 16:49:57 +0200

    Improvement of the code and new file for a custom error

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,7 +1,22 @@
+require_relative 'strands_length_error'
+
 class Hamming
   def self.compute(strand1, strand2)
-    raise StrandLengthError unless strand1.length == strand2.length
+    raise StrandsLengthError unless strand1.length == strand2.length
 
     strand1.chars.zip(strand2.chars).count { |n1, n2| n1 != n2 }
   end
 end
+
+if $PROGRAM_NAME == __FILE__
+  Hamming.compute('a', 'ab')
+end
+
+
+# You will find below, the contents of the file "strands_length_error.rb"
+
+# class StrandsLengthError < StandardError
+#   def initialize(msg="Strands length must be identical") 
+#     super
+#   end
+# end
--- ruby/hamming/strand_length_error.rb
+++
@@ -1,5 +0,0 @@
-class StrandLengthError < StandardError
-  def initialize(msg="Stand lengths must be identical") 
-    super
-  end
-end
