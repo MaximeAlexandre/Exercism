c891c10144ff844656f5dbf8dcf35b10e98b9099
Alexandre Maxime <alexandre.maxime666@gmail.com>
2019-09-23 17:22:05 +0200

    Improvment

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,13 +1,35 @@
 require_relative 'strands_length_error'
 
 class Hamming
-  def self.compute(strand1, strand2)
-    raise StrandsLengthError unless strand1.length == strand2.length
 
-    strand1.chars.zip(strand2.chars).count { |n1, n2| n1 != n2 }
+  attr_reader :strand1, :strand2
+
+  def self.compute(strand1, strand2)
+    new(strand1, strand2).distance
+  end
+
+  def initialize(strand1, strand2)
+    raise StrandsLengthError unless valid?(strand1, strand2)
+
+    @strand1 = strand1
+    @strand2 = strand2
+  end
+
+  def distance
+    nucleotides.count { |n1, n2| n1 != n2 }
+  end
+
+  def nucleotides
+    strand1.chars.zip(strand2.chars)
+  end
+
+  private
+
+  def valid?(strand1, strand2)
+    strand1.length == strand2.length
   end
 end
 
 if $PROGRAM_NAME == __FILE__
-  Hamming.compute('a', 'ab')
+  puts Hamming.compute('def', 'def')
 end
