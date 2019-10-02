660f2369727716df4b03a04e2c57d8fadc667a50
Alexandre Maxime <alexandre.maxime666@gmail.com>
2019-10-02 17:25:07 +0200

    Transpose Improvment

--- ruby/transpose/transpose.rb
+++ ruby/transpose/transpose.rb
@@ -6,19 +6,30 @@ class Transpose
     number_rows = array_input.length
     number_columns = array_input.map(&:length).max
 
-    result = Array.new(array_input.map(&:length).max){ |i| array_input.map{ |e| e[i] } }
+    result = Array.new(number_columns){ |i| array_input.map{ |e| e[i] } }
 
-    for i in 0...number_columns do
-      for j in 0...number_rows - 1 do
-        if result[i][j].nil?
-          for x in 1...number_rows do
-            result[i][j] = " " unless result[i][-x].nil?
-          end
-        end
-      end
-    end
+    # for i in 0...number_columns do
+    #   for j in 0...number_rows - 1 do
+    #     if result[i][j].nil?
+    #       for x in 1...number_rows do
+    #         result[i][j] = " " unless result[i][-x].nil?
+    #       end
+    #     end
+    #   end
+    # end
+    # result
     
-    result.map { |row| row.join }.join("\n")
+    result.map { |row|
+      row.map { |element|
+        if element.nil?
+          # for x in 1...number_rows do
+            element = " " unless row[-1].nil?
+          # end
+        end
+      }
+    }
+
+    # result.map { |row| row.join }.join("\n")
   end
 end
 
