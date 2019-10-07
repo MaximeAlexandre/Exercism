c8396e035feb68147a5d72944f191f08ebd6a432
Alexandre Maxime <alexandre.maxime666@gmail.com>
2019-10-07 17:27:15 +0200

    working on saddle-points

--- ruby/saddle-points/saddle_points.rb
+++ ruby/saddle-points/saddle_points.rb
@@ -1,5 +1,5 @@
 class Matrix
-  attr_reader :rows
+  attr_reader :rows, :saddle_point
 
   def initialize(matrix)
     @rows = matrix_to_rows(matrix)
@@ -14,11 +14,14 @@ class Matrix
   end
 
   def saddle_points
-
+    print max_rows = rows.map { |row| [row.index(row.max), row.max] }
+    # min_columns = columns.map { |column| column.min }
+    # print common_number = (max_rows & min_columns).join.to_i
+    # saddle_point = rows.map.with_index { |row, index| [index, row.index(common_number)] }
   end
 end
 
 if $PROGRAM_NAME == __FILE__
-  matrix = Matrix.new("1 2\n3 4")
-  matrix.saddle_points
+  matrix = Matrix.new("18 3 39 19 91\n38 10 8 77 320\n3 4 8 6 7")
+  print matrix.saddle_points
 end
