8b75ccef57ca28263a905975fb1c408442dba2ba
Alexandre Maxime <alexandre.maxime666@gmail.com>
2019-10-08 17:27:18 +0200

    saddle-points ok

--- ruby/saddle-points/saddle_points.rb
+++ ruby/saddle-points/saddle_points.rb
@@ -1,27 +1,39 @@
 class Matrix
-  attr_reader :rows, :saddle_point
+  attr_reader :rows, :columns
 
   def initialize(matrix)
     @rows = matrix_to_rows(matrix)
+    @columns = rows.transpose
   end
 
   def matrix_to_rows(matrix)
     matrix.each_line.map {|row| row.split.map { |element| element.to_i}}
   end
 
-  def columns
-    rows.transpose
+  def saddle_points
+    common_index.map { |element| [element, rows[element].index(row_max_value[element])] }
   end
 
-  def saddle_points
-    print max_rows = rows.map { |row| [row.index(row.max), row.max] }
-    # min_columns = columns.map { |column| column.min }
-    # print common_number = (max_rows & min_columns).join.to_i
-    # saddle_point = rows.map.with_index { |row, index| [index, row.index(common_number)] }
+  private
+
+  def row_max_value
+    rows.map { |row| row.max }
+  end
+
+  def index_row_max_value
+    rows.map { |row| row.index(row.max) }
+  end
+
+  def column_min_value
+    index_row_max_value.map { |e| columns[e].min }
+  end
+
+  def common_index
+    row_max_value.map.with_index { |x, y| y if x == column_min_value[y] }.compact
   end
 end
 
 if $PROGRAM_NAME == __FILE__
-  matrix = Matrix.new("18 3 39 19 91\n38 10 8 77 320\n3 4 8 6 7")
+  matrix = Matrix.new("4 5 4\n3 5 5\n1 5 4")
   print matrix.saddle_points
 end
